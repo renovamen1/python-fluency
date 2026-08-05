f98c65c76da27029f88a65b57a1469ea9a9dea38
Prabin Thakur <prabinthakur0101@gmail.com>
2026-08-05 15:00:29 +0530

    completed first exercise and added a function to find line no of a word

--- syntax drills/file handling/file handling exercises-1.py
+++ syntax drills/file handling/file handling exercises-1.py
@@ -10,14 +10,14 @@ i want to built some good machine learning models.
 #1(c).search if the word "learning" exsits in the file or not.
 
 # you can open it in 'w' mode,it also creates a file but truncates
-with open("python fluency/syntax drills/file handling/practice.txt","x+") as f:
+with open("python fluency/syntax drills/file handling/practice.txt","w") as f:
     f.write("hi everyone i am prabin thakur\n i am learning file handling or file i/o in Java\n i like programming in Java\n i want to built some good machine learning models")
    
 with open("python fluency/syntax drills/file handling/practice.txt","r+") as g:
     words = g.read()
     updated_words=[]
     for word in words.split():
-        if word in ["java","JAVA","Java"]:
+        if word.strip(".,!") in ["java","JAVA","Java"]:
             updated_words.append("python")
         else:
             updated_words.append(word)
@@ -36,3 +36,24 @@ with open("python fluency/syntax drills/file handling/practice.txt","r") as h:
         print("yes,the word exists")
     else:   
         print("no,the word doesnot exist")
+
+#WAP TO FIND IN WHICH LINE OF THE FILE DOES THE WORD "LEARNING" OCCUR FIRST.
+#PRINT -1 IF NOT FOUND
+
+
+def find_line_no(word):
+    '''
+    function which returns the line number in which a particular word is contained.
+    '''
+    line_no = 1 
+    data = True
+    with open("python fluency/syntax drills/file handling/demo.txt","r") as i:
+        while data:
+            data = i.readline()
+            if word in data:
+                print(line_no)
+                return
+            line_no += 1
+    return -1
+
+find_line_no("first")
