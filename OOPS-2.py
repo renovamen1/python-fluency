0929d2bc5e86024c1038359306c3318dfdf69a53
Prabin Thakur <prabinthakur0101@gmail.com>
2026-08-07 19:37:55 +0530

    covered private and public attributes

--- OOPS-2.py
+++ OOPS-2.py
@@ -16,13 +16,14 @@ class Gun:
         self.cocked = True
         print("target is locked and the bullet is ready to fire")
     def shoot(self):
+        self.aim()
         self.trigger_is_pulled = True
         print("the gun is fired at the target")
 
 
 pistol = Gun()
-pistol.aim() # when we are calling aim, we are only showing little info to the user and hiding the loading bullets and cocking details.
-pistol.shoot() 
+# pistol.aim() # when we are calling aim, we are only showing little info to the user and hiding the loading bullets and cocking details.
+# pistol.shoot() 
 
 
 # Encapsulation
@@ -49,6 +50,41 @@ class Account:
     def print_bal(self):
         print("the current balance is",self.balance)
 
-acc1 = Account(40000,"601A")
-acc1.print_bal()
-acc1.credit(7000)
+# acc1 = Account(40000,"601A")
+# acc1.print_bal()
+# acc1.credit(7000)
+
+
+# del keyword -- used to delete object properties or the object itself.
+class Harvest:
+    def __init__(self,crop_name):
+        self.name = crop_name
+
+harvest1 = Harvest("paddy")
+# print(harvest1.name)
+# to delete a object attribute , use [del object.attribute]
+del harvest1 # to delete the entire object itself
+# print(harvest1.name) # this line now errors as harvest1 as a object was deleted by previous line 
+
+
+#PUBLIC AND PRIVATE ATTRIBUTE 
+# PUBLIC AND PRIVATE  IS ONLY APPARENT ENTITY IN PYTHON UNLIKE JAVA, C++
+#thus its only private(like) attributes and methods
+class Google_acc:
+    def __init__(self,gmail,password):
+        self.gmail = gmail
+        self.__password = password # here we have made this attribute private by using 2 underscores infront of its name
+    def reset_pass(self,new_password):
+        current_pass = self.__password
+        print("the current password is",current_pass)
+        self.__password = new_password
+        print("the new password is set to",self.__password)
+    def __pvt_method(self):
+        print("this is for just to test about making a method private")
+
+google_1 = Google_acc("prab1n@gmail.com","abcd123")
+# print(google_1.gmail)    # this is a public attribute since it can be accessed outside the class
+# print(google_1.password) # this way sensitive data is vulnerable. we dont want our credentials to be exposed 
+# to fix it, we simply make the attribute private by putting (__) 2 underscores infront of attribute name 
+# google_1.reset_pass("changed_pass123") # we can still call & view the password becuase private means it cannot be accessed outside classs but we can infer and call within the class
+# google_1.__pvt_method() # yup, this works. method of object(google_1) is set to private and only can be acccessed with in the class
